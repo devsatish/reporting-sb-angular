2582ad7dff6ba69aa456ace6f262955316f14598
Satish <satish.mummadi@gmail.com>
2025-01-15 15:11:13 -0500

    Adding scaffold

---
+++ src/main/java/com/cvshealth/client_reporting_sb_angular/ClientReportingSbAngularApplication.java
@@ -0,0 +1,13 @@
+package com.cvshealth.client_reporting_sb_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientReportingSbAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientReportingSbAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvshealth/client_reporting_sb_angular/ClientReportingSbAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvshealth.client_reporting_sb_angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientReportingSbAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
